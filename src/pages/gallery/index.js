49e1fae5a12ecc83224936c300cf47798b3d0a0b
Harshitver0511 <154688128+Harshitver0511@users.noreply.github.com>
2024-08-02 23:35:32 +0530

    gallerry modifaction done

--- src/pages/gallery/index.js
+++ src/pages/gallery/index.js
@@ -1,37 +1,71 @@
 import React from 'react';
 import './Gallery.css';
-import img from '../../img/background_img/IMG20230921194023.jpg'
+import img from './sell_bg.png'
+
 import { Link } from 'react-router-dom';
 
-const photos = [
-  "https://images.unsplash.com/photo-1720774531704-dff00e791a55?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  "https://images.unsplash.com/photo-1720774531704-dff00e791a55?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-"https://images.unsplash.com/photo-1720774531704-dff00e791a55?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  ,"https://images.unsplash.com/photo-1721296352946-9386ed562323?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-   "https://images.unsplash.com/photo-1720774531704-dff00e791a55?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    ,"https://images.unsplash.com/photo-1721296352946-9386ed562323?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-      ,"https://images.unsplash.com/photo-1721296352946-9386ed562323?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-,"https://images.unsplash.com/photo-1721296378610-2d107840144f?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-,"https://images.unsplash.com/photo-1720774531704-dff00e791a55?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-,"https://images.unsplash.com/photo-1721296378610-2d107840144f?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-,"https://images.unsplash.com/photo-1721296352946-9386ed562323?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-"https://images.unsplash.com/photo-1720774531704-dff00e791a55?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
- , 
-  // Add more images here
-];
 
 const Gallery = () => {
   return (
     <>
     <div id="gallery">
-    <h1>Gallery</h1>
-    <div className="gallery">
-    {photos.map((photo, index) => (
-      <div key={index} className="gallery-item">
-        <img src={photo} alt={`Gallery item ${index + 1}`} />
+      <p className='h11'> Gallery</p>
+      <div className="gallery1">
+        <div className='gallerydiv'>
+          <div className='imgdiv'>
+          <img src={img} alt="img" className='imgg'/>
+          </div>
+          <div className='imgdiv'>
+          <img src={img} alt="img" className='imgg'/>
+          </div>
+          <div className='imgdiv'>
+          <img src={img} alt="img" className='imgg'/>
+          </div>
+          
+        </div>
+        <div className='gallerydiv'>
+          <div className='imgdiv'>
+          <img src={img} alt="img" className='imgg'/>
+          </div>
+          <div className='imgdiv'>
+          <img src={img} alt="img" className='imgg'/>
+          </div>
+          <div className='imgdiv'>
+          <img src={img} alt="img" className='imgg'/>
+          </div>
+
+          
+        </div>
+        <div className='gallerydiv'>
+          <div className='imgdiv'>
+          <img src={img} alt="img" className='imgg'/>
+          </div>
+          <div className='imgdiv'>
+          <img src={img} alt="img" className='imgg'/>
+          </div>
+          <div className='imgdiv'>
+          <img src={img} alt="img" className='imgg'/>
+          </div>
+
+          
+        </div>
+        <div className='gallerydiv'>
+          <div className='imgdiv'>
+          <img src={img} alt="img" className='imgg'/>
+          </div>
+          <div className='imgdiv'>
+          <img src={img} alt="img" className='imgg'/>
+          </div>
+          <div className='imgdiv'>
+          <img src={img} alt="img" className='imgg'/>
+          </div>
+
+          
+        </div>
+        
+
       </div>
-    ))}
-  </div>
+    
   <Link to="/gallery" className="bttnn">View More</Link>
   </div>
   </>
