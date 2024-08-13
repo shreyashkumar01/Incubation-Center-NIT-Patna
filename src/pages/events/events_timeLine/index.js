4c1653df93efeff488d026194dc52dc8a58c1bee
Arpita Dwivedi <arpitad.ug22.cs@nitp.ac.in>
2024-08-13 17:11:52 +0530

    Added some more content

--- src/pages/events/events_timeLine/index.js
+++ src/pages/events/events_timeLine/index.js
@@ -16,10 +16,12 @@ function Timeline() {
       <div class="container left-container">
             <img src=" img/IC LOGO (1).png " alt='logo'/>
             <div class="text-box">
-                <h2>Event name1</h2>
-                <small>timings1</small>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus,
-                    necessitatibus repellendus? Delectus porro laborum dolores maiores explicabo iste minima? Dolor?
+                <h2>Orientation Session 2023</h2>
+                <small>21/09/2023</small>
+                <p>The orientation program aimed to familiarize the students of 2023 
+batch with the Incubation Centre (IC) at NIT Patna and its pivotal role 
+in cultivating entrepreneurship among students while fostering 
+innovation. 
                 </p>
                 
             </div>
@@ -28,10 +30,13 @@ function Timeline() {
         <div class="container right-container">
         <img src=" img/IC LOGO (1).png " alt='logo'/>
             <div class="text-box">
-                <h2>Event name2</h2>
-                <small>timings2</small>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus,
-                     necessitatibus repellendus? Delectus porro laborum dolores maiores explicabo iste minima? Dolor?
+                <h2>PitchTember 2023</h2>
+                <small>24/11/2023 to 18/12/2023</small>
+                <p>It is a multitude of events, including Udhyamita, Uthaan, Unnayan, 
+and workshops, facilitated extensive participation from students 
+across various universities. With 1000+ students presenting 67+ 
+innovative ideas, the event celebrated excellence by recognizing 
+noteworthy contributions.
                 </p>
                 <span class="right-container-arrow"></span>
             </div>
@@ -39,10 +44,15 @@ function Timeline() {
         <div class="container left-container">
         <img src=" img/IC LOGO (1).png " alt='logo'/>
             <div class="text-box">
-                <h2>Event name3</h2>
-                <small>timings3</small>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus,
-                    necessitatibus repellendus? Delectus porro laborum dolores maiores explicabo iste minima? Dolor?
+                <h2>Stock Market Simulation</h2>
+                <small> 09/02/2024</small>
+                <p>The      IPSIT      Stock      Market       Simulation, sponsored by StockGro, provided 
+participants with an immersive and educational experience in 
+virtual stock trading without financial risk. Each participant was 
+allocated 10 lakh virtual money for a day of trading on actual 
+stocks. Almost 300 participants showcased their interest, 
+emphasizing the importance of effective trading strategies in 
+achieving success. 
                 </p>
                 <span class="left-container-arrow"></span>
             </div>
