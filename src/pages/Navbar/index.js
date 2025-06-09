6823a650f954fbcacce21332b2e112e03dbf8feb
MANIKANTA PERNEEDI <130591134+mk6281@users.noreply.github.com>
2025-06-09 13:08:08 +0530

    Update index.js

--- src/pages/Navbar/index.js
+++ src/pages/Navbar/index.js
@@ -79,7 +79,7 @@ const Navbar = () => {
           </div>
           <a href="#events" className='link'>Events</a>
           <a href="#gallery" className='link'>Gallery</a>
-          // <a href="#footer" className='link'>Contact</a>
+          {/* <a href="#footer" className='link'>Contact</a>*/}
           <a href="src/img/background_img/Rulebook.pdf" className='link' download="Tinkering Lab">Tinkering lab</a>
           <Link className='link' to="/team">Team</Link> 
 
@@ -97,7 +97,7 @@ const Navbar = () => {
                 <a href="#gallery" className='link'>Gallery</a>
                 <a href="src/img/background_img/Rulebook.pdf" className='link' download="Tinkering Lab">Tinkering lab</a>
                 <a href="#team" className='link'>Team</a> 
-                // <a href="#footer" className='link'>Contact</a>
+  {/*<a href="#footer" className='link'>Contact</a>
               </div>
               <div onClick={() => setIsOpen(false)} className="crossBtn">
                 X
