1d950ed7162f239698bd75b5633158e5bb9d4df6
Sairaj182 <sairajraithatha.official@gmail.com>
2025-06-15 15:51:55 +0530

    fix: incubate u'r idea is now functional and recieving idea mails (on email-id: toic.nitp.temp@gmail.com). This set-up is done using web3 forms service

--- src/pages/idea/index.js
+++ src/pages/idea/index.js
@@ -2,57 +2,47 @@ import React from 'react';
 import "./style.css";
 import IdeaJs from './idea.js';
 
-
 const Idea = () => {
-  // const handleSubmit = (e) => {
-  //   IdeaJs(e); // Call IdeaJs and pass the event
-  // };
-
   return (
     <div>
-      <section class="idea" id="idea"> 
+      <section className="idea" id="idea">
         <h1 data-aos="fade-down">Incubate u'r Idea</h1>
-        <div className='form' data-aos="fade-up"> 
+        <div className='form' data-aos="fade-up">
           <form action="#" onSubmit={IdeaJs}>
 
-            <div class="input-box">
-              <div class="input-field field">
-                <input type="text" placeholder="Full Name" id="name" class="item" autocomplete="off" />
-                <div class="error-txt">Full Name can't be blank</div>
+            <div className="input-box">
+              <div className="input-field field">
+                <input type="text" placeholder="Full Name" id="name" name="name" className="item" autoComplete="off" />
+                <div className="error-txt">Full Name can't be blank</div>
               </div>
-              <div class="input-field field">
-                <input type="text" placeholder="Email Address " id="email" class="item" autocomplete="off" />
-                <div class="error-txt email">Email Address can't be blank</div>
-              </div>
-            </div>
-            <div class="input-box">
-              <div class="input-field field">
-                <input type="text" placeholder="Phone Number" id="phone" class="item" autocomplete="off" />
-                <div class="error-txt"> Phone Number can't be blank</div>
-              </div>
-              <div class="input-field field">
-                <input type="text" placeholder="subject  " id="subject" class="item" autocomplete="off" />
-                <div class="error-txt">subject can't be blank</div>
+              <div className="input-field field">
+                <input type="text" placeholder="Email Address" id="email" name="email" className="item" autoComplete="off" />
+                <div className="error-txt email">Email Address can't be blank</div>
               </div>
             </div>
 
-            <div class="textarea-field field">
-              <textarea name="" id="message" cols="30" rows="6" class="item" placeholder="Your Message"
-                autocomplete="off"></textarea>
-              <div class="error-txt">Message can't be blank</div>
+            <div className="input-box">
+              <div className="input-field field">
+                <input type="text" placeholder="Phone Number" id="phone" name="phone" className="item" autoComplete="off" />
+                <div className="error-txt">Phone Number can't be blank</div>
+              </div>
+              <div className="input-field field">
+                <input type="text" placeholder="Subject" id="subject" name="subject" className="item" autoComplete="off" />
+                <div className="error-txt">Subject can't be blank</div>
+              </div>
             </div>
 
-            <button type="submit"  >Submit</button>
+            <div className="textarea-field field">
+              <textarea id="message" name="message" cols="30" rows="6" className="item" placeholder="Your Message" autoComplete="off"></textarea>
+              <div className="error-txt">Message can't be blank</div>
+            </div>
+
+            <button type="submit">Submit</button>
           </form>
         </div>
-        
       </section>
-      <script src="https://smtpjs.com/v3/smtp.js"></script>
-     
-      <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    
     </div>
-  )
-}
+  );
+};
 
 export default Idea;
